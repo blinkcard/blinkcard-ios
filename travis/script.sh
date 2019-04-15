5c72faae1cb5ef7cbb3dbddd0601b9c4d290aa5e
Jure Čular <jure.cular@microblink.com>
2019-04-15 12:43:31 +0200

    Added travis build script

---
+++ travis/script.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+xcodebuild -project Samples/BlinkCard-Sample-ObjC/BlinkCard-Sample-ObjC.xcodeproj \
+    -scheme BlinkCard-Sample-ObjC \
+    -configuration Debug \
+    -sdk iphonesimulator \
+    ONLY_ACTIVE_ARCH=NO \
+    clean build
+
+xcodebuild -project Samples/BlinkCard-Sample-ObjC/BlinkCard-Sample.xcodeproj \
+    -scheme BlinkCard-Sample-ObjC \
+    -configuration Release \
+    -sdk iphonesimulator \
+    ONLY_ACTIVE_ARCH=NO \
+    clean build
+
+ xcodebuild -project Samples/BlinkCard-Sample-Swift/BlinkCard-Sample-Swift.xcodeproj \
+    -scheme BlinkCard-Sample-Swift \
+    -configuration Debug \
+    -sdk iphonesimulator \
+    ONLY_ACTIVE_ARCH=NO \
+    clean build
+
+xcodebuild -project Samples/BlinkCard-Sample-Swift/BlinkCard-Sample-Swift.xcodeproj \
+    -scheme BlinkCard-Sample-Swift \
+    -configuration Release \
+    -sdk iphonesimulator \
+    ONLY_ACTIVE_ARCH=NO \
+    clean build    
