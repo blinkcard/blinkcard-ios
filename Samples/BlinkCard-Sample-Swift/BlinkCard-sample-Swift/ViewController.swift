baf9249f720710e44844a43d578b99f770b6ae12
Mijo Gracanin <mijo.gracanin@gmail.com>
2022-06-25 22:42:27 +0200

    Update samples with the new license for v2.6.0

--- Samples/BlinkCard-Sample-Swift/BlinkCard-sample-Swift/ViewController.swift
+++ Samples/BlinkCard-Sample-Swift/BlinkCard-sample-Swift/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         
         var licenseErrorMessage = ""
         
-        // Valid until: 2022-03-15
+        // Valid until: 2022-10-19
         MBCMicroblinkSDK.shared().setLicenseResource("license", withExtension: "txt", inSubdirectory: "", for: .main) { (licenseError) in
             switch licenseError {
             case .invalidLicense:
